3572c0d89092a3610633974958bdbe80413c66ba
Leilamena <lmena@udesa.edu.ar>
2026-03-12 15:49:27 -0300

    def analizar_habitos ()
    recibe lista.

--- funciones_habitos.py
+++ funciones_habitos.py
@@ -1,7 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 12 15:22:47 2026
-
-@author: delfi
-"""
-
+def analizar_habitos (lista): 
+    dicc_final={}
+    for actividad in lista: 
+     if actividad in dicc_final:
+            dicc_final[actividad] += 1
+     else:
+            dicc_final[actividad] = 1
+    return dicc_final 
