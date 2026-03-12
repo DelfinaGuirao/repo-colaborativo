8816228b0b45c7cf94260050f84a66851a29abc8
Emiliawelyczko <ewelyczko@udesa.edu.ar>
2026-03-12 16:06:00 -0300

    def registrar_habitos()
    a ver.

--- funciones_habitos.py
+++ funciones_habitos.py
@@ -1,7 +1,29 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 12 15:22:47 2026
+<<<<<<< HEAD
 
-@author: delfi
-"""
 
+def registrar_habitos():
+   lista_habitos=[]
+   actividades = input("Ingrese las actividades: ")
+   lista_habitos.append(actividades)
+  
+   while True:
+        actividades_agregadas = input("Desea seguir agregando?: ")
+      
+        if actividades_agregadas == "si":
+          actividades = input("Ingrese las actividades: ") 
+          lista_habitos.append(actividades)
+    
+        elif actividades_agregadas == "no":
+             break 
+   return lista_habitos
+        
+=======
+def analizar_habitos (lista): 
+    dicc_final={}
+    for actividad in lista: 
+     if actividad in dicc_final:
+            dicc_final[actividad] += 1
+     else:
+            dicc_final[actividad] = 1
+    return dicc_final 
+>>>>>>> c26329f303170f573da17e56d97e933863857235
--- programa_principal.py
+++ programa_principal.py
@@ -4,4 +4,11 @@ Created on Thu Mar 12 15:22:47 2026
 
 @author: delfi
 """
+import funciones_habitos
 
+lista = funciones_habitos.registrar_habitos()
+
+resultado = funciones_habitos.analizar_habitos(lista)
+
+print("Resumen de actividades:")
+print(resultado)
